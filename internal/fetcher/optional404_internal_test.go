0b8e49174e1de6d29745b09c41e8ebb28e76cdae
sindreh-navikt <224779700+sindreh-navikt@users.noreply.github.com>
2026-04-23 09:37:49 +0200

    test(fetcher): activate optional404 tests

--- internal/fetcher/optional404_internal_test.go
+++ internal/fetcher/optional404_internal_test.go
@@ -5,55 +5,58 @@ import (
 	"fmt"
 	"net/http"
 	"net/http/httptest"
+	"strings"
+	"testing"
 	"time"
-
-	. "github.com/onsi/ginkgo/v2"
-	. "github.com/onsi/gomega"
 )
 
-var _ = Describe("doRequestWithRateLimitAndOptional404", func() {
-	var originalClient *http.Client
-	var originalBackoff func(int) time.Duration
-
-	BeforeEach(func() {
-		originalClient = HttpClient
-		originalBackoff = RetryBackoff
-		RetryBackoff = func(_ int) time.Duration { return time.Millisecond }
-	})
-
-	AfterEach(func() {
+func TestDoRequestWithRateLimitAndOptional404ReturnsNilOn404(t *testing.T) {
+	originalClient := HttpClient
+	originalBackoff := RetryBackoff
+	t.Cleanup(func() {
 		HttpClient = originalClient
 		RetryBackoff = originalBackoff
 	})
+	RetryBackoff = func(_ int) time.Duration { return time.Millisecond }
 
-	It("skal returnere nil ved 404 (ressurs ikke tilgjengelig)", func() {
-		ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			w.WriteHeader(http.StatusNotFound)
-			_, _ = fmt.Fprintln(w, `{"message":"not found"}`)
-		}))
-		defer ts.Close()
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusNotFound)
+		_, _ = fmt.Fprintln(w, `{"message":"not found"}`)
+	}))
+	defer ts.Close()
 
-		HttpClient = ts.Client()
-		ctx := context.Background()
-		var result any
-		err := doRequestWithRateLimitAndOptional404(ctx, "GET", ts.URL, "token", nil, &result)
+	HttpClient = ts.Client()
+	ctx := context.Background()
+	var result any
+	err := doRequestWithRateLimitAndOptional404(ctx, "GET", ts.URL, "token", nil, &result)
+	if err != nil {
+		t.Fatalf("expected nil error on optional 404, got %v", err)
+	}
+}
 
-		Expect(err).ToNot(HaveOccurred())
+func TestDoRequestWithRateLimitAndOptional404ReturnsErrorOnNon404Failure(t *testing.T) {
+	originalClient := HttpClient
+	originalBackoff := RetryBackoff
+	t.Cleanup(func() {
+		HttpClient = originalClient
+		RetryBackoff = originalBackoff
 	})
+	RetryBackoff = func(_ int) time.Duration { return time.Millisecond }
 
-	It("skal returnere feil ved andre ikke-2xx statuskoder", func() {
-		ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			w.WriteHeader(http.StatusForbidden)
-			_, _ = fmt.Fprintln(w, `{"message":"forbidden"}`)
-		}))
-		defer ts.Close()
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusForbidden)
+		_, _ = fmt.Fprintln(w, `{"message":"forbidden"}`)
+	}))
+	defer ts.Close()
 
-		HttpClient = ts.Client()
-		ctx := context.Background()
-		var result any
-		err := doRequestWithRateLimitAndOptional404(ctx, "GET", ts.URL, "token", nil, &result)
-
-		Expect(err).To(HaveOccurred())
-		Expect(err.Error()).To(ContainSubstring("403"))
-	})
-})
+	HttpClient = ts.Client()
+	ctx := context.Background()
+	var result any
+	err := doRequestWithRateLimitAndOptional404(ctx, "GET", ts.URL, "token", nil, &result)
+	if err == nil {
+		t.Fatal("expected error on non-404 failure")
+	}
+	if !strings.Contains(err.Error(), "403") {
+		t.Fatalf("expected error to mention 403, got %v", err)
+	}
+}
